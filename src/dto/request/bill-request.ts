287040f7a9a710e37eb65f1bb7966e55f8647a0e
XisUndefined <muhlisahmadabdillah_1306619028@mhs.unj.ac.id>
2024-05-27 19:43:05 +0700

    fix: fix CreateBillRequest dto to match zod validation

--- src/dto/request/bill-request.ts
+++ src/dto/request/bill-request.ts
@@ -1,9 +1,10 @@
-import { EnumStatusBill } from '../../enums/status-bill-enum';
+import { EnumStatusBill } from "../../enums/status-bill-enum";
 
 export interface CreateBillRequest {
   user_id: number;
   bill_type_id: number;
   status: EnumStatusBill;
+  quanity: number;
 }
 
 export interface UpdateBillRequest {
